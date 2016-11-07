16c681aeeae1bf22870b7438f4f564434751bd1c
teemofeyrs <teemofeyrs@gmail.com>
2016-11-07 14:26:48 +0200

    init commit

--- fitness_js/js/javascript.js
+++ fitness_js/js/javascript.js
@@ -6,12 +6,12 @@ var liSide = document.createElement('li');
 liSide.innerHTML = '<a hfer="#">О Нас</a>';
 leftPanel.appendChild(liSide);
 
-var col = document.querySelector('.side_menu li a');
-console.dir(col);
-col.style.color = 'green';//почему-то цвет меняет первый элемент
+var col = document.querySelectorAll('.side_menu li a');
+    for (var i = 0; i < col.length; i++)
+       col[i].style.color = "green";
 
 var text = document.getElementsByTagName('p');
-text[0].innerHTML = "позволяет делать что угодно с HTML-элементом и его содержимым, но для этого нужно сначала нужный элемент получить,Доступ к DOM начинается с объекта document. Из него можно добраться до любых узлов.Так выглядят основные ссылки, по которым можно переходить между узлами DOM.";//текс не изменился
+text[0].innerHTML = "позволяет делать что угодно с HTML-элементом и его содержимым, но для этого нужно сначала нужный элемент получить,Доступ к DOM начинается с объекта document. Из него можно добраться до любых узлов.Так выглядят основные ссылки, по которым можно переходить между узлами DOM.";
 
 var copy = document.getElementsByClassName('menu_main menu_main_bottom clearfix');
 console.log(copy[0].childNodes[9].outerText);
