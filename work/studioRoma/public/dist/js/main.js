3fb1b9c5cdc4352f92bc54f25927ad2e6f3e1e8c
teemofey <teemofeyrs@gmail.com>
2019-03-15 20:22:54 +0200

    media + slow scroll

---
+++ work/studioRoma/public/dist/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+   /*slow scroll*/
+    $('.listNav').click(function(e) {
+        var listItem = $(this).attr('href');
+     $("html, body").animate({
+         scrollTop: $(listItem).offset().top
+     }, 1000);
+        e.preventDefault();
+    });
+});
