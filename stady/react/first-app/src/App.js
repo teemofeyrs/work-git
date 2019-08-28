877bd0940ddf745e997887c6c65f6ae4e7be1e9e
teemofey <teemofeyrs@gmail.com>
2019-08-28 21:01:34 +0300

    react study

---
+++ stady/react/first-app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './Style.css';
+import Nav from './Nav';
+import Content from './Content';
+
+const App = () => {
+  return (
+    <div className="App">
+      <Nav />
+      <Content />
+    </div>
+  );
+};
+export default App;
---
+++ stady/react/first-app/src/Content.js
@@ -0,0 +1,15 @@
+import React from 'react';
+const Content = () => {
+    return (
+        <div>
+            <div clasName="Baner">
+                <div className="logo">
+
+                </div>
+            </div>
+            <h2>I'm Shmatchenko Artem</h2>
+            <div className="arrow"></div>
+        </div>
+    );
+}
+export default Content;
